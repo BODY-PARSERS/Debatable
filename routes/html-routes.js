6ec7700728918481627c0bf5bf20eeaccebbf710
davidbinstock <binstock.d@gmail.com>
2018-05-30 22:32:19 -0500

    sign up works!

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,5 +11,9 @@ module.exports = function(app){
     app.get("/", function(request, response){
         response.sendFile(path.join(__dirname,"../public/home.html"))
     });
+    // Sign-In Route
+    app.get("/sign-in", function(request, response){
+        response.sendFile(path.join(__dirname,"../public/login.html"))
+    });
 
 };
