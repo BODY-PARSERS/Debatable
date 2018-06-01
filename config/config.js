ff5e503bb187d74c1aef860067eaea4c27f4177c
davidbinstock <binstock.d@gmail.com>
2018-05-31 21:52:02 -0500

    moved database user info to .env file - also added handlebars

---
+++ config/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "development": {
+      "username": process.env.USERNAME,
+      "password": process.env.PASSWORD,
+      "database": process.env.DATABASE,
+      "host": "127.0.0.1",
+      "port": 3306,
+      "dialect": "mysql"
+    },
+    "test": {
+      "username": "root",
+      "password": null,
+      "database": "database_test",
+      "host": "127.0.0.1",
+      "port": 3306,
+      "dialect": "mysql"
+    },
+    "production": {
+      "use_env_variable": "JAWSDB_URL",
+      "dialect": "mysql"
+    }
+  }
