94da01ae662078dc93cfcdc3fefaa19ce4868403
rmiranda11 <r_miranda_11@yahoo.com>
2018-06-06 18:49:10 -0500

    debate

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -52,4 +52,16 @@ module.exports = function(app){
         })
     });
 
+    //----------------------------------Debate MSG-------------------
+app.get('/debate', function(req, res){
+    res.sendFile(__dirname + '../debatePage.html');
+    });
+  
+      io.on('connection', function(socket){
+        socket.on('chat message', function(msg){
+            io.emit('chat message', msg);
+        });
+      });
+  //----------------------------------------------------------
+
 };
