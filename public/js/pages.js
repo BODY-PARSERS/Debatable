8131d88dd8a78490fb413272a46ea75224a2ba19
Guan <heguanelvis@gmail.com>
2018-06-06 13:34:43 -0500

    added log off button and restrict some pages for visitors

--- public/js/pages.js
+++ public/js/pages.js
@@ -1,39 +1,88 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Changing Page Session//
-////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Changing Page Session//
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     var userId = sessionStorage.getItem("userId")
     var userName = sessionStorage.getItem("userName")
     $("#name-display").html(userName)
 
 
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-    $(document).on("click", "#user-home-page-dropdown",function(event){
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Nav Bar on-click events
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    // Go to user homepage
+    $(document).on("click", "#user-home-page-dropdown", function (event) {
         console.log("you clicked go to user home page");
-        if(userId){
+        if (userId) {
             location.href = "/userhome"
-        }else{
+        } else {
             alert("Please Sign In");
             location.href = "/login"
         }
-
     })
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Log off
+    $(document).on("click", "#log-off-dropdown", function (event) {
+        console.log("you clicked log off");
+        if (userId) {
+            sessionStorage.removeItem("userId")
+            sessionStorage.removeItem("userName")
+            alert("Logged off successfully!")
+            location.href = "/"
+        } else {
+            alert("Please Sign In");
+            location.href = "/login"
+        }
+    })
+
+    // Create Debate Button
+    $(document).on("click", "#create-debate-button", function (event) {
+        console.log("you clicked create");
+        if (userId) {
+            location.href = "/createdebate"
+        } else {
+            alert("Please Sign In");
+            location.href = "/login"
+        }
+    })
+
+    // Join Debate Button
+    $(document).on("click", "#join-debate-button", function (event) {
+        console.log("you clicked join");
+        if (userId) {
+            location.href = "/join"
+        } else {
+            alert("Please Sign In");
+            location.href = "/login"
+        }
+    })
+
+    // Continue Debate Button
+    $(document).on("click", "#continue-debate-button", function (event) {
+        console.log("you clicked continue");
+        if (userId) {
+            location.href = "/continue"
+        } else {
+            alert("Please Sign In");
+            location.href = "/login"
+        }
+    })
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     // Handling Sign Up Form Submission
-    $(document).on("submit", "#sign-up-form", function(event){
+    $(document).on("submit", "#sign-up-form", function (event) {
         event.preventDefault();
         console.log("you clicked submit!")
         var nameInput = $("#username-input").val().trim();
         var emailInput = $("#email-input").val().trim();
         var passwordInput = $("#password-input").val().trim();
 
-        console.log(nameInput,emailInput,passwordInput);
+        console.log(nameInput, emailInput, passwordInput);
 
         var newUser = {
             username: nameInput,
@@ -46,10 +95,10 @@ $(document).ready(function(){
 
 
         $.post("/api/users", newUser)
-            .then( function(result){
+            .then(function (result) {
                 console.log("User Added!");
                 console.log(result);
-            }).catch(function(error){
+            }).catch(function (error) {
                 console.log("There was an error:")
                 console.log(error)
             })
@@ -57,32 +106,32 @@ $(document).ready(function(){
     })
 
     // Handling Log In Form Submission
-    $(document).on("submit", "#log-in-form", function(event){
+    $(document).on("submit", "#log-in-form", function (event) {
         event.preventDefault();
         console.log("you clicked submit!")
         var nameInput = $("#log-in-username").val().trim();
         var passwordInput = $("#log-in-password").val().trim();
 
-        console.log(nameInput,passwordInput);
+        console.log(nameInput, passwordInput);
 
         $("#log-in-username").val("");
         $("#log-in-password").val("");
 
 
-        $.get("/api/users/"+nameInput+"/"+passwordInput)
-            .then( function(result){
+        $.get("/api/users/" + nameInput + "/" + passwordInput)
+            .then(function (result) {
                 console.log(result);
-                if(result){
+                if (result) {
                     console.log("Login succesful!");
                     console.log("You are logged in as: ", result.username);
                     sessionStorage.setItem('userId', result.id)
                     sessionStorage.setItem('userName', result.username)
-                }else{
+                } else {
                     console.log("login failed....");
                     alert("Username and/or Password Incorrect")
                 }
                 location.href = "/userhome"
-            }).catch(function(error){
+            }).catch(function (error) {
                 console.log("There was an error:")
                 console.log(error)
             })
@@ -101,9 +150,12 @@ $(document).ready(function(){
             topic: topicInput,
         }
 
+        $("#topic-input").val("");
+
         $.post("/api/debates", newDebate)
             .then(function (result) {
                 console.log("Debate Added!");
+                alert("Debate Created")
                 console.log(result);
 
             }).catch(function (error) {
@@ -118,8 +170,8 @@ $(document).ready(function(){
         event.preventDefault();
         console.log("you clicked join!")
         var debateId = $(this).data("debateid");
-        console.log("debate ID: ",debateId);
-        console.log("your user ID: ",userId);
+        console.log("debate ID: ", debateId);
+        console.log("your user ID: ", userId);
 
         // console.log(topicInput);
 
